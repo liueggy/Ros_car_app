bb43832583343969379d3be90a39fb88a6553890
Eggy <eggy@minis.local>
2026-05-18 15:36:16 +0800

    ios: add missing robot controlling protocol methods

--- ios/EggyRobotClient/Sources/RobotControlling.swift
+++ ios/EggyRobotClient/Sources/RobotControlling.swift
@@ -12,14 +12,6 @@ protocol RobotControlling: AnyObject {
     func toggleExplore()
     func saveMap(name: String)
     func reset()
-    func startMapping()
-    func stopMapping()
-    func saveNavigationMap(name: String)
-    func startQuickDirectNavigation()
-    func startMoveBaseNavigation(mapPath: String?)
-    func stopNavigation()
-    func setMode(_ mode: String)
-    func sendRawCommand(_ dict: [String: Any])
 }
 
 extension RobotViewModel: RobotControlling {
@@ -27,5 +19,4 @@ extension RobotViewModel: RobotControlling {
     var isRobotOnline: Bool { robotOnline }
     var frontDistance: Double? { state?.summary.front }
     var isAutoExploreRunning: Bool { state?.system.autoExplore == true }
-    func sendRawCommand(_ dict: [String: Any]) { send(dict) }
 }
